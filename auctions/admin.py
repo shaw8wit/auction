52eddd4f4d2e098f49dab73cb9ea8999f6b3e680
shaw8wit <shaw8wit@gmail.com>
2020-08-10 12:15:07 +0530

    Database configured

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, AuctionListing, User, Comment, Bid
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -6,8 +6,42 @@ class User(AbstractUser):
     pass
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=32)
 
     def __str__(self):
         return f"{self.id} : {self.name}"
+
+
+class AuctionListing(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=16)
+    date = models.DateTimeField()
+    startBid = models.DecimalField(decimal_places=2, max_digits=5)
+    description = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} : {self.name} in {self.category.name}\nPosted at : {self.date}\nValue : {self.startBid}\nDescription : {self.description}\nPosted By : {self.user.username}"
+
+
+class Bid(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bidValue = models.DecimalField(decimal_places=2, max_digits=5)
+    auctionListing = models.ForeignKey(
+        AuctionListing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id} : {self.user.username} bid {self.bidValue} on {self.auctionListing.name} at {self.date}"
+
+
+class Comment(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auctionListing = models.ForeignKey(
+        AuctionListing, on_delete=models.CASCADE)
+    commentValue = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.id} : {self.user.username} commented on {self.auctionListing.name} posted by {self.auctionListing.user.username} at {self.date} : {self.commentValue}"
